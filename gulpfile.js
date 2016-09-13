8db204473038b1938c534cfc8ad5e697be776026
Peter Manuel <peterjohn.manuel@gmail.com>
2016-09-13 07:26:27 +0200

    Sass
    Installed and configured sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,39 @@
+/** Gulp Libraries */
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 var browserSync = require('browser-sync').create();
 
 
-/** Directory */
-var directory = {
-    baseDirectory: './src/',
-    distDirectory: './dist/'
+/** Directories */
+var directories = {
+    baseDir: './src/',
+    bootstrapDir: './bower_components/bootstrap-sass',
+    cssDir: 'css/',
+    distDir: './dist/',
+    sassDir: 'scss/**/*.scss'
 }
 
-
+/** Sass  */
 gulp.task('sass', function(){
-    gulp.src('scss/*.scss')
+    gulp.src(directories.baseDir + directories.sassDir)
     .pipe(sass())
-    .pipe(gulp.dest('css'))
+    .pipe(sass({ includePaths: [config.bootstrapDir + '/assets/stylesheets'] }))
+    .pipe(gulp.dest(directories.baseDir + directories.cssDir))
     .pipe(browserSync.reload({stream: true}));
 });
 
-
+/** Browser Sync */
 gulp.task('browser-sync', function(){
     browserSync.init({
         server : { 
-            baseDir: directory.baseDirectory
+            baseDir: directories.baseDirectory
         }
     });
 });
 
+/** Watch */
 gulp.task('watch', ['browser-sync'], function(){
 
     gulp.watch("./src/scss/*.scss", ['sass']);
