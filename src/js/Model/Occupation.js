f69c32ab98fa7d1a9118d86b3b53da32af6e8b13
Peter Manuel <peterjohn.manuel@gmail.com>
2016-09-15 13:51:06 +0200

    Formatted layout.

--- src/js/Model/Occupation.js
+++ src/js/Model/Occupation.js
@@ -16,8 +16,8 @@
     self.endDate = endDate;
 }
 
-var occupation1 = new Occupation('Process Computing Technology (PCT)', 'Internship', 'July 2012', 'December 2012');
-var occupation2 = new Occupation('Process Computing Technology (PCT)', 'Junior Developer ', 'July 2013', 'December 2015');
+var occupation1 = new Occupation('Process Computing Technology', 'Internship', 'July 2012', 'December 2012');
+var occupation2 = new Occupation('Process Computing Technology', 'Junior Developer ', 'July 2013', 'December 2015');
 var occupation3 = new Occupation('RedPanda Software', 'Developer', 'December 2015', 'Current');
 
 var allOccupations = [occupation3, occupation2, occupation1];
