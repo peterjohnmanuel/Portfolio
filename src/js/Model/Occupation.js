fda5cfe33ec0bdeb4a6a492d530945903f09145f
Peter John Manuel <peterjohn.manuel@gmail.com>
2016-09-18 23:33:34 +0200

    Fixed occupation dates.

--- src/js/Model/Occupation.js
+++ src/js/Model/Occupation.js
@@ -17,7 +17,7 @@
 }
 
 var occupation1 = new Occupation('Process Computing Technology', 'Internship', 'July 2012', 'December 2012');
-var occupation2 = new Occupation('Process Computing Technology', 'Junior Developer ', 'July 2013', 'December 2015');
+var occupation2 = new Occupation('Process Computing Technology', 'Junior Developer ', 'January 2013', 'December 2015');
 var occupation3 = new Occupation('RedPanda Software', 'Developer', 'December 2015', 'Current');
 
 var allOccupations = [occupation3, occupation2, occupation1];
