4380c608a505ec762c74f5feeca4cc00396837f9
Peter Manuel <peterjohn.manuel@gmail.com>
2016-12-06 22:17:14 +0200

    Removed no longer required view models.

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,12 @@
 (function(){
 'use strict';
 
-var tech = new TechnologyViewModel();
-var occ = new OccupationViewModel();
 var det = new DetailViewModel();
-var edu = new EducationViewModel();
 var cont = new ContactViewModel();
 var proj = new ProjectViewModel();
 
 
-ko.applyBindings(tech, document.getElementById('technology'));
-ko.applyBindings(occ, document.getElementById('occupation'));
 ko.applyBindings(det, document.getElementById('user'));
-ko.applyBindings(edu, document.getElementById('education'));
 ko.applyBindings(cont, document.getElementById('contact'));
 ko.applyBindings(proj, document.getElementById('projects'));
 
