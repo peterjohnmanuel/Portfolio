04d6c6c212b713e482a81cf2d20af9c78bf71ac8
Peter Manuel <peterjohn.manuel@gmail.com>
2016-12-06 22:18:52 +0200

    Refactored Contact
    - Added contactIcon
    - Added isHyperlink
    - Added additional details for contact information.

--- src/js/Model/Contact.js
+++ src/js/Model/Contact.js
@@ -1,14 +1,21 @@
-/**
- * Holds all contact information
+/** Contact 
  * @class Contact
- */
-
-function Contact(contactDetail, contactIcon){
+ * @param contactType
+ * @param contactDetails
+ * @param contactIcon
+*/
+function Contact(contactType, contactDetails ,contactIcon, isHyperlink){
     var self = this;
-    this.contactDetail = contactDetail;
-    this.contactIcon = contactIcon;
+
+    self.contactType = contactType;
+    self.contactDetails = contactDetails;
+    self.contactIcon = contactIcon;
+    self.isHyperlink = isHyperlink;
 }
+var contact1 = new Contact('Yahoo', 'peterjohn.manuel@yahoo.com', 'fa-yahoo', false);
+var contact2 = new Contact('Skype', 'peterjohn.manuel', 'fa-skype', false);
+var contact3 = new Contact('Github', 'https://github.com/peterjohnmanuel', 'fa-github', true);
+var contact4 = new Contact('Location', 'Cape Town, RSA', 'fa-map-o', false);
+var contact5 = new Contact('Resume', 'http://manueloverride.ddns.net:8090/Resume', 'fa-briefcase', true);
 
-var contact = new Contact("peterjohn.manuel@yahoo.com", "fa-yahoo");
-
-var contacts = [contact];
+var contacts = [contact1,contact2, contact3, contact4, contact5];
